2fc87bf7adb0163b10c783270be61554580e9608
oblakov0372 <demaoblak1@gmail.com>
2024-04-01 01:20:13 +0300

    create user resource

---
+++ tucha-planner-backend/src/user/user.dto.ts
@@ -0,0 +1,41 @@
+import {
+	IsEmail,
+	IsNumber,
+	IsOptional,
+	IsString,
+	Max,
+	Min,
+	MinLength
+} from 'class-validator'
+
+export class PomodoroSettingsDto {
+	@IsOptional()
+	@IsNumber()
+	@Min(1)
+	workInterval?: number
+
+	@IsOptional()
+	@IsNumber()
+	@Min(1)
+	breakInterval?: number
+
+	@IsOptional()
+	@IsNumber()
+	@Min(1)
+	@Max(10)
+	intervalsCount?: number
+}
+
+export class UserDto extends PomodoroSettingsDto {
+	@IsOptional()
+	@IsEmail()
+	email: string
+
+	@IsOptional()
+	@MinLength(6, {
+		message: 'Password must be at least 6 characters long'
+	})
+	@IsOptional()
+	@IsString()
+	password: string
+}
--- tucha-planner-backend/src/user/user.module.ts
+++ tucha-planner-backend/src/user/user.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
 import { UserService } from './user.service'
 import { UserController } from './user.controller'
-import { PrismaService } from 'src/prisma.service'
 
 @Module({
 	controllers: [UserController],
